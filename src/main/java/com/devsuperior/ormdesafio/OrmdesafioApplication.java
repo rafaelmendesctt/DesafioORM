f1b275421d8da64b24faf901dc25dd837e39e319
Rafael Mendes <rafaelmendesctt@gmail.com>
2024-04-17 16:09:50 -0300

    Desafio de domínio e ORM com H2

---
+++ src/main/java/com/devsuperior/ormdesafio/OrmdesafioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ormdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmdesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmdesafioApplication.class, args);
+	}
+
+}
